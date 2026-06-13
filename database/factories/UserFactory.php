3ad00127536b535ef660a36f69431c223b12a573
Abdalrhman Almustafa <161914020+DarkZero81@users.noreply.github.com>
2026-06-13 14:47:21 +0300

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,15 +24,18 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        // استخدام دالة المولد العام المباشرة لضمان عدم رجوع قيمة null على السيرفر
+        $faker = \Faker\Factory::create();
+
         return [
-            // تم استبدال fake() بـ $this->faker لضمان استقرار الحقن السحابي
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
+
 
     /**
      * Indicate that the model's email address should be unverified.
